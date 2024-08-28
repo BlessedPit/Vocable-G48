19c8db2806f8169aeb95f6a6537a92de4c6b07cc
AnitaGranita <anita.scortegagna@studenti.unitn.it>
2024-08-28 18:21:14 +0200

    awop

--- vocable_app_1/FrontEnd/src/views/ForgotPassword.vue
+++ vocable_app_1/FrontEnd/src/views/ForgotPassword.vue
@@ -1,23 +1,66 @@
 <template>
-    <div class="wrapper">
-        <v-sheet class="pa-12 mx-auto elevation-2" rounded width="400">
+    <div class="wrapper d-flex align-center justify-center">
+        <v-sheet class="pa-6 elevation-2" rounded max-width="500px" width="100%">
             <v-form ref="form">
-                <img src="/logoVuoto.png" alt="Vocable Logo" />
-                <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Vocable</span>
-
-                <v-text-field class="required" type="email" :rules="emailRules" v-model="email"
+                <div class="text-center mb-4">
+                    <img src="/logoVuoto.png" alt="Vocable Logo" class="logo-img" />
+                    <span class="self-center text-3xl font-semibold whitespace-nowrap dark:text-white">Password dimenticata</span>
+                    <v-card-text class="text-h6">Inserisci il tuo indirizzo email, dopodiché ti arriverà una mail che ti chiederà di resettare la password.</v-card-text>
+                </div>
+                
+                <v-text-field class="required ma-5" type="email" :rules="emailRules" v-model="email"
                     label="Email" variant="underlined"></v-text-field>
 
-                <div class="d-flex flex-column">
-                    <v-btn class="mt-4" color="blue" size="large" variant="elevated" block @click="sendResetLink">
+                <div class="d-flex flex-column mt-5 mb-5">
+                    <v-btn class="mt-4 button" color="blue-500" size="x-large" variant="elevated"  rounded="xl" block @click="sendResetLink">
                         Invia link di reset
                     </v-btn>
-
                 </div>
             </v-form>
         </v-sheet>
     </div>
 </template>
+
+<style scoped>
+.wrapper {
+    min-height: 96vh;
+    padding: 16px;
+    background: linear-gradient(135deg, rgba(89, 158, 255, 0.2), rgb(1, 43, 255)); 
+    align-items: center;
+    justify-content: center;
+}
+
+
+.logo-img {
+    max-width: 100px;
+    height: auto;
+    margin: 0 auto;
+    margin-bottom: 16px;
+}
+
+.button{
+    transition: background-image 0.4s ease, transform 0.4s ease;
+    color: white;
+}
+.button:hover{
+    transform: translateY(-4px);
+}
+.button:active{
+    transform: translateY(-2px); 
+}
+
+@media (max-width: 600px) {
+    .wrapper {
+        padding: 8px;
+    }
+    .v-sheet {
+        padding: 16px !important;
+    }
+    .logo-img {
+        max-width: 80px;
+    }
+}
+</style>
 
 <script>
 import axios from 'axios';
